d83bf46c73f3bf37dc6e1de9f445809ff6963dda
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2022-09-16 14:12:47 -0300

    Update box.js

--- box.js
+++ box.js
@@ -2,5 +2,6 @@ function clikck(){
     document.getElementById("c").value = document.getElementById("a").value
     document.getElementById("e").value = document.getElementById("b").value
     document.getElementById("f").value = document.getElementById("d").value
+    alert('Seu cadastro foi realizado com sucesso')
 } 
 
